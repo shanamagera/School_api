2bb238ee32536aaed1fe32e60e0ea99d23ad5811
shanamagera <shanamagera@gmail.com>
2023-12-01 11:36:20 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/create-course', [CourseController::class, 'createCourse']);
 Route::get('/getCourses', [CourseController::class, 'readAllCourses']);
-Route::get('/getCourse', [CourseController::class, 'readCourse']);
-Route::put('/updateCourse', [CourseController::class, 'updateCourse']);
-Route::delete('/deleteCourse', [CourseController::class, 'deleteCourse']);
+Route::get('/getCourse/{id}', [CourseController::class, 'readCourse']);
+Route::put('/updateCourse/{id}', [CourseController::class, 'updateCourse']);
+Route::delete('/deleteCourse/{id}', [CourseController::class, 'deleteCourse']);
 
 
